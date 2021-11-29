ad00ca34a9f8c8c87dfd8f6e2d6fda051906615a
Alex <alexis.amand@gmail.com>
2021-11-29 11:39:44 +0100

    [maj] Nouveau footer

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,23 +1,41 @@
-<footer class="text-muted py-5">
-  <div class="container">
+<!-- Lien de retour en haut de la page -->
+
+<div class="container">
 
     <div class="row">
-        <p class="text-end mb-1">
-            <a href="#">Retour en haut</a>
+        <p class="text-end my-3 backToTop">
+            <a href="#"><i class="bi bi-arrow-bar-up me-1"></i>Retour en haut</a>
         </p>
     </div>
-    
-    <div class="row mt-3 footer-inside">
-        <div class="col-8">
-            <p class="mb-1">Le Bock Trotter v. 0.2.177 &copy; <a href="https://www.boitasite.com">Boitasite</a> - 2021</p>
-            <!--p class="mb-0">N'hésitez pas à visiter mes sites sur <a href="http://www.genealexis.fr">la généalogie</a> et sur <a href="http://histoiresdepoilus.genealexis.fr">la Grande Guerre</a>.</p-->
+
+</div>
+
+<footer class="py-5">
+  <div class="container">    
+    <div class="row justify-content-between mt-3">
+        <div class="col-12 col-md-5 text-start">
+
+            <h4>A Propos</h4> 
+
+            <hr class="py-0 my-4 text-white me-5">
+
+            <a href="#">Me contacter</a><br />
+            <a href="#">Mentions légales</a><br />
+            <a href="#">Bibliographie</a><br />
+
+            <p class="mb-1 mt-4">Le Bock Trotter v. 0.3.2 &copy; <a href="https://www.boitasite.com">Boitasite</a> - 2021</p>
         </div>
-        <div class="col-4 text-end fs-3">    
-            <i class="bi bi-facebook"></i>
-            <i class="bi bi-facebook"></i>
-            <i class="bi bi-facebook"></i>
+
+        <div class="col-12 col-md-4 text-end mt-5 mt-md-0">    
+
+            <h4>Me suivre</h4> 
+
+            <hr class="py-0 my-4 text-white ms-5">
+
+            <a href="#"><i class="bi bi-facebook fs-2 mx-2"></i></a>
+            <a href="#"><i class="bi bi-twitter fs-2 mx-2"></i></a>
+            <a href="#"><i class="bi bi-instagram fs-2 mx-2"></i></a>
         </div>
     </div>
-
   </div>
 </footer>
